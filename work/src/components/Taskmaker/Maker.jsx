13056f754ffd4e7c5181e75ef0ba0b11864d199b
jero <jeroariasm@gmail.com>
2024-04-04 03:47:35 -0500

    3th commit

--- work/src/components/Taskmaker/Maker.jsx
+++ work/src/components/Taskmaker/Maker.jsx
@@ -5,22 +5,33 @@ import './Maker.css'
 export const Maker = () =>
 
  {
-    const [task, settask] = useState('')
 
     return(
         <> 
+
             <div className='makework'>
-                <input className='smbhomework' placeholder='Set a task' type="text" />
+                <input className='smbhomework' placeholder='Set a task' type="text"/>
                 <input className='smbhomework' placeholder='Describe u task' type="text" />
                 <input onClick={ ()=> settask(task)} className='butthomework' type="button" value="Make Homework" />
+
+                    
+            <div className='reportmsj'>
+                <h2>You have (X) tasks pending and (X) completed.</h2>
+            </div>
+
                 <hr className='linespace'/>
             </div>
 
-            <div className='Workslist'>
-                <ul>
-                    <li>{task}</li>
-                </ul>
+            <div className='filter'>           
+
+                <select id="state">
+                <option label="All"></option>
+                <option label="Completed"></option>
+                <option label="Pending"></option>
+                </select>
+
             </div>
+
         </>
     )
  }
